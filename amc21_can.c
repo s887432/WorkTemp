00379a48740b14e06bfad558ab7834c94009dd79
Patrick Lin <patrick.cclin@gmail.com>
2024-09-18 20:52:00 +0800

    Create amc21_can.c

---
+++ amc21_can.c
@@ -0,0 +1,211 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+*******************************************************************************/
+
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+uint8_t Can1MessageRAM[CAN0_MESSAGE_RAM_CONFIG_SIZE] __attribute__((aligned (32)));
+
+/* Standard identifier id[28:18]*/
+#define WRITE_ID(id) (id << 18)
+#define READ_ID(id)  (id >> 18)
+
+//static uint32_t status = 0;
+static uint8_t loop_count = 0;
+//static uint8_t user_input = 0;
+
+static uint8_t txFiFo[CAN0_TX_FIFO_BUFFER_SIZE];
+//static uint8_t rxFiFo0[CAN0_RX_FIFO0_SIZE];
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local functions
+// *****************************************************************************
+// *****************************************************************************
+/* Menu */
+#if 0
+void print_menu(void)
+{
+    printf(" ------------------------------ \r\n");   
+    printf(" Press '1' to Transmit message \r\n");
+    printf(" Press 'm' to Display menu \r\n");
+}
+/* Print Rx Message */
+static void print_message(uint8_t numberOfMessage, CAN_RX_BUFFER *rxBuf, uint8_t rxBufLen)
+{
+    uint8_t length = 0;
+    uint8_t msgLength = 0;
+    uint32_t id = 0;
+
+    for (uint8_t count = 0; count < numberOfMessage; count++)
+    {
+        /* Print message to Console */
+        printf(" Rx FIFO0 : New Message Received\r\n");
+        id = rxBuf->xtd ? rxBuf->id : READ_ID(rxBuf->id);
+        msgLength = rxBuf->dlc;
+        length = msgLength;
+        printf(" Message - ID : 0x%x Length : 0x%x ", (unsigned int)id, (unsigned int)msgLength);
+        printf("Message : ");
+        while(length)
+        {
+            printf("0x%x ", rxBuf->data[msgLength - length--]);
+        }
+        printf("\r\n");
+        rxBuf += rxBufLen;
+    }
+}
+#endif
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    CAN_TX_BUFFER *txBuffer = NULL;
+//    uint8_t        numberOfMessage = 0;
+
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    SYSTICK_TimerStart();
+
+    printf(" ------------------------------ \r\n");
+    printf("            CAN Demo            \r\n");
+    printf(" ------------------------------ \r\n");
+    
+    /* Set Message RAM Configuration */
+    CAN0_MessageRAMConfigSet(Can1MessageRAM);
+
+//    print_menu();
+    
+    while ( true )
+    {
+#if 0        
+        /* Rx FIFO0 */
+        if (CAN0_InterruptGet(CAN_INTERRUPT_RF0N_MASK))
+        {    
+            CAN0_InterruptClear(CAN_INTERRUPT_RF0N_MASK);
+
+            /* Check CAN Status */
+            status = CAN0_ErrorGet();
+
+            if (((status & CAN_PSR_LEC_Msk) == CAN_ERROR_NONE) || ((status & CAN_PSR_LEC_Msk) == CAN_ERROR_LEC_NC))
+            {
+                numberOfMessage = CAN0_RxFifoFillLevelGet(CAN_RX_FIFO_0);
+                if (numberOfMessage != 0)
+                {
+                    memset(rxFiFo0, 0x00, (numberOfMessage * CAN0_RX_FIFO0_ELEMENT_SIZE));
+                    if (CAN0_MessageReceiveFifo(CAN_RX_FIFO_0, numberOfMessage, (CAN_RX_BUFFER *)rxFiFo0) == true)
+                    {
+                        print_message(numberOfMessage, (CAN_RX_BUFFER *)rxFiFo0, CAN0_RX_FIFO0_ELEMENT_SIZE);
+                    }
+                    else
+                    {
+                        printf(" Error in received message\r\n");
+                    }
+                }
+            }
+            else
+            {
+                printf(" Error in received message\r\n");
+            }
+        }
+
+        /* User input */
+        if (SERCOM0_USART_ReceiverIsReady() == false)
+        {
+            continue;
+        }
+        user_input = (uint8_t)SERCOM0_USART_ReadByte();
+        user_input = '1';
+#endif
+//        switch (user_input)
+//        {
+//            case '1': 
+                
+                printf(" Transmitting Message:");
+                memset(txFiFo, 0x00, CAN0_TX_FIFO_BUFFER_ELEMENT_SIZE);
+                txBuffer = (CAN_TX_BUFFER *)txFiFo;
+                txBuffer->id = WRITE_ID(0x469);
+                txBuffer->dlc = 8;
+                for (loop_count = 0; loop_count < 8; loop_count++){
+                    txBuffer->data[loop_count] = loop_count+1;
+                }                
+                if (CAN0_MessageTransmitFifo(1, txBuffer) == true)
+                {    
+                    printf(" Success \r\n");
+                }
+                else
+                {
+                    printf(" Failed \r\n");
+                }             
+                
+                SYSTICK_DelayMs(1000);
+//                break;
+//            case 'm':
+//            case 'M':
+//                print_menu();
+//                break;
+//            default:
+//                printf(" Invalid Input \r\n");
+//                break;
+//        }
+        
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+End of File
+*/
